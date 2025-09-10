3c17fbca5e347ce96df42d0d50df3795cd57fef0
harshvikram777 <harshvikrams507@gmail.com>
2025-09-10 16:23:37 +0530

    new updates

---
+++ function.js
@@ -0,0 +1,19 @@
+
+
+function getComputerMove() {
+    const randomNumber = Math.random();
+    let computerMove = '';
+    if (randomNumber > 0 && randomNumber < 1/3) 
+        {
+            computerMove = 'rock';
+
+
+    }
+    else if (randomNumber >= 1/3 && randomNumber < 2/3){
+        computerMove = 'paper';
+    }
+    else(randomNumber >= 2/3 && randomNumber < 1) 
+    {
+        computerMove = 'scissors';
+}
+}
